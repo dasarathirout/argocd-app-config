79598284f5fc3879aaac11fbbcd96ac3b74069f5
R.Dasarathi <dasarathi.rout@live.com>
2023-03-04 17:28:51 +0530

    M004: MVN Content & Chart Name CleanUp

---
+++ src/main/java/io/k8s/drout/MainDriver.java
@@ -0,0 +1,10 @@
+package io.k8s.drout;
+
+
+public class MainDriver 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "MainDriver Running ..." );
+    }
+}
---
+++ src/test/java/io/k8s/drout/DriverMainTest.java
@@ -0,0 +1,11 @@
+package io.k8s.drout;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class DriverMainTest
+{
+    @Test
+    public void checkK8ArgoCD()
+    {
+        Assertions.assertTrue(true,"Place Holder Test Method Runs");
+    }
+}
